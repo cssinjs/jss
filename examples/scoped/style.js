332d412e80e41a2b4ca965d1bc2e5398567679d6
Oleg Slobodskoi <oleg008@gmail.com>
2014-10-19 22:26:18 +0200

    fix stringified version of rule, don't add nested rules

--- examples/scoped/style.js
+++ examples/scoped/style.js
@@ -1,21 +1,22 @@
 (function() {
 
-var rules = {}
-
-rules['.square'] = {
-    float: 'left',
-    width: 100,
-    height: 100,
-    '& button': {
-        padding: 20,
-        background: 'blue'
-    },
-    '&.red': {
-        background: 'red'
+var rules = {
+    '.square': {
+        float: 'left',
+        width: 100,
+        height: 100,
+        '& button': {
+            padding: 20,
+            background: 'blue'
+        },
+        '&.red': {
+            background: 'red'
+        }
     }
 }
 
 // Application
-jss.createStyle(rules).attach()
+console.log(jss.createStyle(rules).attach())
+
 
 }())
