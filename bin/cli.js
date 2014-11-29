2a58e53bc96468106a7d8c39d4230df82af35c9c
Oleg Slobodskoi <oleg008@gmail.com>
2014-11-29 16:09:22 +0100

    refactor cli #12

--- bin/cli.js
+++ bin/cli.js
@@ -1,66 +1,8 @@
 #!/usr/bin/env node
 
-var css = require('css')
 var fs = require('fs')
 var path = require('path')
 var program = require('commander')
-
-/**
- * Convert rules from css ast to jss style.
- *
- * @param {Array} cssRules
- * @return {Object}
- */
-function toJss(cssRules) {
-    var jssRules = {}
-
-    function addRule(rule, rules) {
-        var key, style = {}
-        key = rule.selectors.join(', ')
-        rule.declarations.forEach(function (decl) {
-            style[decl.property] = decl.value
-        })
-        rules[key] = style
-    }
-
-    cssRules.forEach(function (rule) {
-        switch (rule.type) {
-            case 'rule':
-                addRule(rule, jssRules)
-                break
-            case 'media':
-                var key = '@media ' + rule.media
-                var value = {}
-                rule.rules.forEach(function(rule) {
-                    addRule(rule, value)
-                })
-                jssRules[key] = value
-                break
-            case 'font-face':
-                var key = '@' + rule.type
-                var value = {}
-                rule.declarations.forEach(function (decl) {
-                    value[decl.property] = decl.value
-                })
-                jssRules[key] = value
-                break
-            case 'keyframes':
-                var key = '@' + rule.type + ' ' + rule.name
-                var value = {}
-                rule.keyframes.forEach(function (keyframe) {
-                    var frameKey = keyframe.values.join(', ')
-                    var frameValue = {}
-                    keyframe.declarations.forEach(function (decl) {
-                        frameValue[decl.property] = decl.value
-                    })
-                    value[frameKey] = frameValue
-                })
-                jssRules[key] = value
-        }
-    })
-
-    return jssRules;
-}
 
 program
     .version(require('../package.json').version)
@@ -80,15 +22,15 @@ program.parse(process.argv)
 
 if (!program.args.length) return program.help()
 
+var cssToJss = require('./cssToJss')
+
 ;(function convert() {
     var code = fs.readFileSync(program.sourcePath, 'utf-8')
-    var ast = css.parse(code)
+    var ext = path.extname(program.sourcePath)
 
-    if (ast.stylesheet && ast.stylesheet.rules) {
-        var jss = toJss(ast.stylesheet.rules)
-        var spaces = program.pretty ? '  ' : ''
-        var output = JSON.stringify(jss, null, spaces)
-        output = program.export + output + ';'
-        console.log(output)
+    switch (ext) {
+        case '.css':
+            console.log(cssToJss(code, program))
+            break
     }
 }())
---
+++ bin/cssToJss.js
@@ -0,0 +1,85 @@
+'use strict'
+
+var css = require('css')
+
+/**
+ * Convert css to jss.
+ *
+ * Options:
+ *   - `pretty` pretty formatting
+ *   - `export` namespace string to add before output
+ *
+ * @param {String} code
+ * @param {Object} options
+ * @return {String}
+ * @api public
+ */
+module.exports = function (code, options) {
+    var ast = css.parse(code)
+
+    if (ast.stylesheet && ast.stylesheet.rules) {
+        var jss = toJssRules(ast.stylesheet.rules)
+        var spaces = options.pretty ? '  ' : ''
+        var output = JSON.stringify(jss, null, spaces)
+        return options.export + output + ';'
+    }
+
+    return ''
+}
+
+/**
+ * Convert rules from css ast to jss style.
+ *
+ * @param {Array} cssRules
+ * @return {Object}
+ */
+function toJssRules(cssRules) {
+    var jssRules = {}
+
+    function addRule(rule, rules) {
+        var key, style = {}
+        key = rule.selectors.join(', ')
+        rule.declarations.forEach(function (decl) {
+            style[decl.property] = decl.value
+        })
+        rules[key] = style
+    }
+
+    cssRules.forEach(function (rule) {
+        switch (rule.type) {
+            case 'rule':
+                addRule(rule, jssRules)
+                break
+            case 'media':
+                var key = '@media ' + rule.media
+                var value = {}
+                rule.rules.forEach(function(rule) {
+                    addRule(rule, value)
+                })
+                jssRules[key] = value
+                break
+            case 'font-face':
+                var key = '@' + rule.type
+                var value = {}
+                rule.declarations.forEach(function (decl) {
+                    value[decl.property] = decl.value
+                })
+                jssRules[key] = value
+                break
+            case 'keyframes':
+                var key = '@' + rule.type + ' ' + rule.name
+                var value = {}
+                rule.keyframes.forEach(function (keyframe) {
+                    var frameKey = keyframe.values.join(', ')
+                    var frameValue = {}
+                    keyframe.declarations.forEach(function (decl) {
+                        frameValue[decl.property] = decl.value
+                    })
+                    value[frameKey] = frameValue
+                })
+                jssRules[key] = value
+        }
+    })
+
+    return jssRules;
+}
