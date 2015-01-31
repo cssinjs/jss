ac837e66a8fd346c6627b4122bdf4c76e0d99031
Oleg Slobodskoi <oleg008@gmail.com>
2015-01-31 21:13:39 +0100

    fix example

--- examples/composition/app.js
+++ examples/composition/app.js
@@ -1,11 +1,10 @@
 (function () {
     var sheetA = jss.createStyleSheet(window.componentA).attach()
-    console.log(sheetA.toString())
-    //var sheetB = jss.createStyleSheet(window.componentB).attach()
+    var sheetB = jss.createStyleSheet(window.componentB).attach()
 
     var tpl = document.getElementById('template').innerHTML
 
     document.body.innerHTML = tpl
         .replace('{button-a}', sheetA.classes.button)
-        //.replace('{button-b}', sheetB.classes.button)
+        .replace('{button-b}', sheetB.classes.button)
 }())
