c56b4168bb98614257ab553aebcdbadba8eb5e1e
Joseph Orbegoso Pea <joe@trusktr.io>
2015-05-29 13:21:41 -0700

    Add jss to global

--- index.js
+++ index.js
@@ -5,5 +5,22 @@
  * @website https://github.com/jsstyles/jss
  * @license MIT
  */
+ 
+var lib = require('./lib/index')
+var root
 
-module.exports = require('./lib/index')
+if (typeof window == 'object' && this === window) {
+    root = window
+}
+else if (typeof global == 'object' && this === global) {
+    root = global
+}
+else {
+    root = this
+}
+
+if (root != null) {
+  root.jss = lib
+}
+
+module.exports = lib
