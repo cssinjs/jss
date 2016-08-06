5623fb3a0d530beaf92a33c862a2a36ad8521cfd
Oleg Slobodskoi <oleg008@gmail.com>
2016-08-07 01:43:28 +0200

    fix webpack config

--- packages/jss-preset-default/history.md
+++ packages/jss-preset-default/history.md
@@ -1,3 +1,7 @@
+## 0.1.1 / 2016-08-07
+
+- fix webpack config
+
 ## 0.1.0 / 2016-08-06
 
 - first release
--- packages/jss-preset-default/webpack.config.js
+++ packages/jss-preset-default/webpack.config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   output: {
-    library: 'jssCamelCase',
+    library: 'jssPreset',
     libraryTarget: 'umd'
   },
   plugins: plugins,
