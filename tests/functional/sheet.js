780fd03513c8797facdf87c746793d68debde1d5
nathanmarks <info@nathanmarks.io>
2016-09-02 18:35:18 -0400

    Fix lint errors

--- tests/functional/sheet.js
+++ tests/functional/sheet.js
@@ -138,9 +138,9 @@ describe('Functional: sheet', () => {
 
     it('should be set by the options argument', () => {
       [-50, 0, 50, 9999].forEach((n) => {
-        const sheet2 = jss.createStyleSheet({}, { index: n })
+        const sheet2 = jss.createStyleSheet({}, {index: n})
         expect(sheet2.options.index).to.be(n)
-      });
+      })
     })
   })
 
