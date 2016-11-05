3ebcbffb42f5b68687d1230a821fab9610c38437
Oleg Slobodskoi <oleg008@gmail.com>
2016-11-05 19:14:49 +0100

    add jss-props-sort and jss-vendor prefixer to the tests suite

--- tests/index.js
+++ tests/index.js
@@ -5,6 +5,8 @@ import 'jss-default-unit/tests/index.test'
 import 'jss-camel-case/tests/index.test'
 import 'jss-extend/tests/index.test'
 import 'jss-isolate/tests/index.test'
+import 'jss-props-sort/tests/index.test'
+import 'jss-vendor-prefixer/tests/index.test'
 import 'react-jss/tests/index.test'
 
 import './integration/jss'
