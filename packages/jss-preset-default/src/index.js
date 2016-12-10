2c2c185b97931a2972d4a1abc7b7749dfa4d7c72
Oleg Slobodskoi <oleg008@gmail.com>
2016-12-10 01:50:52 +0100

    release 1.1.0

--- packages/jss-preset-default/src/index.js
+++ packages/jss-preset-default/src/index.js
@@ -6,9 +6,11 @@ import vendorPrefixer from 'jss-vendor-prefixer'
 import propsSort from 'jss-props-sort'
 import compose from 'jss-compose'
 import expand from 'jss-expand'
+import global from 'jss-global'
 
 export default (options = {}) => ({
   plugins: [
+    global(options.global),
     extend(options.extend),
     nested(options.nested),
     compose(options.compose),
