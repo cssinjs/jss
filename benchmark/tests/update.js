dc7be78e096ef2b52bb20ec44c804059ab92eb03
Oleg Slobodskoi <oleg008@gmail.com>
2017-03-16 17:09:52 +0200

    add sheet.update() bench

---
+++ benchmark/tests/update.js
@@ -0,0 +1,30 @@
+import {create, sheets} from 'jss'
+
+function teardown() {
+  this.sheet.detach()
+  this.sheets.reset()
+}
+
+function createSheet() {
+  let colorSwitch = true
+  return create().createStyleSheet({
+    button: {
+      color: () => {
+        // Need to change color each time to avoid caching.
+        colorSwitch = !colorSwitch
+        return colorSwitch ? 'green' : 'red'
+      }
+    }
+  }, {link: true}).attach()
+}
+
+suite('Update', () => {
+  benchmark('sheet.update()', function benchmark() {
+    if (!this.sheet) {
+      this.sheet = createSheet()
+      this.sheets = sheets
+    }
+
+    this.sheet.update()
+  }, {teardown})
+})
