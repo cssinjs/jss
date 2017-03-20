e5045aa144608bc5298b6606feb3057c5e919237
Oleg Slobodskoi <oleg008@gmail.com>
2017-03-20 15:26:31 +0200

    implement onChangeValue hook

--- src/types.js
+++ src/types.js
@@ -43,7 +43,8 @@ export interface Rule {
 export type Plugin = {
   onCreateRule?: (name: string, decl: Object, options: RuleOptions) => Rule|null,
   onProcessRule?: (rule: Rule, sheet?: StyleSheet) => void,
-  onProcessSheet?: (sheet?: StyleSheet) => void
+  onProcessSheet?: (sheet?: StyleSheet) => void,
+  onChangeValue?: (value: string, prop: string, rule: Rule) => string
 }
 
 export type JssOptions = {
@@ -79,7 +80,7 @@ export type StyleSheetOptions = {
   jss: Jss
 }
 
-export type StyleSheetInstanceOptions = {
+export type InternalStyleSheetOptions = {
   media?: string,
   meta?: string,
   index: number,
