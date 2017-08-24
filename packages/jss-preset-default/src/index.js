9919c0cd7014fb6bb922825590e6bec8cb8b9a43
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-08-24 16:13:29 +0300

    Sorted plugin list, with according to official plugin list

--- packages/jss-preset-default/src/index.js
+++ packages/jss-preset-default/src/index.js
@@ -1,7 +1,7 @@
+import template from 'jss-template'
 import global from 'jss-global'
 import extend from 'jss-extend'
 import nested from 'jss-nested'
-import template from 'jss-template'
 import compose from 'jss-compose'
 import camelCase from 'jss-camel-case'
 import defaultUnit from 'jss-default-unit'
@@ -11,10 +11,10 @@ import propsSort from 'jss-props-sort'
 
 export default (options = {}) => ({
   plugins: [
+    template(options.template),
     global(options.global),
     extend(options.extend),
     nested(options.nested),
-    template(options.template),
     compose(options.compose),
     camelCase(options.camelCase),
     defaultUnit(options.defaultUnit),
