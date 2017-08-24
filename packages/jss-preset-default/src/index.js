d1ad011bedddd7c5168078b3dc53dc5ce34e325c
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-08-24 16:05:43 +0300

    Added jss-template, and updated dependencies

--- packages/jss-preset-default/src/index.js
+++ packages/jss-preset-default/src/index.js
@@ -1,18 +1,20 @@
+import global from 'jss-global'
 import extend from 'jss-extend'
 import nested from 'jss-nested'
+import template from 'jss-template'
+import compose from 'jss-compose'
 import camelCase from 'jss-camel-case'
 import defaultUnit from 'jss-default-unit'
+import expand from 'jss-expand'
 import vendorPrefixer from 'jss-vendor-prefixer'
 import propsSort from 'jss-props-sort'
-import compose from 'jss-compose'
-import expand from 'jss-expand'
-import global from 'jss-global'
 
 export default (options = {}) => ({
   plugins: [
     global(options.global),
     extend(options.extend),
     nested(options.nested),
+    template(options.template),
     compose(options.compose),
     camelCase(options.camelCase),
     defaultUnit(options.defaultUnit),
