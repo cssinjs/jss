b4c786295ce1264942e7f75ab4860502ad62d3fa
Brenton Simpson <appsforartists@google.com>
2017-10-31 11:22:40 -0700

    fix flow types for Observable rule plugin

--- src/plugins/observables.js
+++ src/plugins/observables.js
@@ -5,19 +5,33 @@ import createRule from '../utils/createRule'
 import type {Observable, Rule, RuleOptions, JssStyle} from '../types'
 
 export default {
-  onCreateRule(name: string, decl: JssStyle | Observable<{[string]: string}>, options: RuleOptions) {
+  onCreateRule(name: string, decl: JssStyle, options: RuleOptions): Rule | null {
     if (isObservable(decl)) {
-      const rule = createRule(name, {}, options);
+      // Cast `decl` to `Observable`, since it passed the type guard
+      const props$ = (decl: Observable<{[string]: string}>);
 
-      decl.subscribe(
-        (currentProps) => {
-          Object.entries(currentProps).forEach(
-            ([key, value]) => rule.prop(key, value)
+      // We know `rule` is a `StyleRule`, and the other types don't have a
+      // `prop` method, so we must explicitly cast to `StyleRule`
+      const rule = ((createRule(name, {}, options): any): StyleRule);
+
+      // `stream.subscribe()` returns a subscription, which should be explicitly
+      // unsubscribed from when we know this sheet is no longer needed, but I
+      // don't see any hooks to do that in the plugin API.  The Observable props
+      // implementation doesn't store its subscription either, so this is a TODO
+      // for both.
+      props$.subscribe(
+        (props) => {
+          Object.entries(props).forEach(
+            // The typing here is gross because `Object.entries` is typed as
+            // `[string, mixed]`, even though `props` is `{[string]: string}`
+            ([key, value]) => rule.prop(key, ((value: any): string))
           );
         }
       );
 
       return rule;
+    } else {
+      return null;
     }
   },
 
