5c2c04044566f2b78e18d53f8c7d18c6fe503b1a
Brenton Simpson <appsforartists@google.com>
2017-10-31 10:34:44 -0700

    add support for Observable rules
    Fixes #607

--- src/plugins/observables.js
+++ src/plugins/observables.js
@@ -1,9 +1,26 @@
 /* @flow */
 import isObservable from 'is-observable'
 import StyleRule from '../rules/StyleRule'
-import type {Rule} from '../types'
+import createRule from '../utils/createRule'
+import type {Observable, Rule, RuleOptions, JssStyle} from '../types'
 
 export default {
+  onCreateRule(name: string, decl: JssStyle | Observable<{[string]: string}>, options: RuleOptions) {
+    if (isObservable(decl)) {
+      const rule = createRule(name, {}, options);
+
+      decl.subscribe(
+        (currentProps) => {
+          Object.entries(currentProps).forEach(
+            ([key, value]) => rule.prop(key, value)
+          );
+        }
+      );
+
+      return rule;
+    }
+  },
+
   onProcessRule(rule: Rule) {
     if (!(rule instanceof StyleRule)) return
     const {style} = rule
