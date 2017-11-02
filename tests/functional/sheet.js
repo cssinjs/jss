4a5cedd6c4e5b2e69f869afe3b3d618b134930c5
Oleg Slobodskoi <oleg008@gmail.com>
2017-11-02 23:16:39 +0100

    split function values and observables tests

--- tests/functional/sheet.js
+++ tests/functional/sheet.js
@@ -257,45 +257,11 @@ describe('Functional: sheet', () => {
       expect(getCss(style)).to.be(removeWhitespace(sheet.toString()))
     })
 
-    it('should render @media with function values', () => {
+    it('should render @media and added rule', () => {
       sheet.addRule('b', {
         color: 'red'
       })
-      sheet.addRule('@media screen', {
-        b: {
-          color: props => (props.primary ? 'black' : 'white')
-        }
-      })
-      sheet.update({primary: true})
       expect(getCss(style)).to.be(removeWhitespace(sheet.toString()))
-    })
-  })
-
-  describe('.addRule() with just function values and attached sheet', () => {
-    let style
-    let sheet
-
-    beforeEach(() => {
-      sheet = jss.createStyleSheet().attach().link()
-      sheet.addRule('a', {color: ({color}) => color})
-      style = getStyle()
-    })
-
-    afterEach(() => {
-      sheet.detach()
-    })
-
-    it('should render an empty rule', () => {
-      expect(getCss(style)).to.be(removeWhitespace(sheet.toString()))
-    })
-
-    it('should render rule with updated color', () => {
-      sheet.update({color: 'red'})
-      expect(sheet.toString()).to.be(stripIndent`
-        .a-id {
-          color: red;
-        }
-      `)
     })
   })
 
@@ -375,48 +341,6 @@ describe('Functional: sheet', () => {
       sheet = jss.createStyleSheet().attach()
       sheet.addRule('%%%%', {color: 'red'})
       expect(warned).to.be(true)
-    })
-  })
-
-  describe('.addRule() with function values for rules from plugins queue', () => {
-    let style
-    let sheet
-
-    beforeEach(() => {
-      jss.use({
-        onProcessRule(rule, ruleSheet) {
-          const ruleName = 'plugin-rule'
-
-          if (rule.key === ruleName) return
-
-          ruleSheet.addRule(ruleName, {
-            color: props => props.color
-          })
-        }
-      })
-      sheet = jss.createStyleSheet({}, {link: true}).attach()
-    })
-
-    afterEach(() => {
-      sheet.detach()
-    })
-
-    it('should render color for rule by plugin', () => {
-      sheet.addRule('rule', {
-        color: props => props.color
-      })
-      sheet.update({color: 'red'})
-      style = getStyle()
-
-      expect(sheet.toString()).to.be(stripIndent`
-        .rule-id {
-          color: red;
-        }
-        .plugin-rule-id {
-          color: red;
-        }
-      `)
-      expect(getCss(style)).to.be(removeWhitespace(sheet.toString()))
     })
   })
 
