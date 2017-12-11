3a20e774b1f0d68e59c17c5764ae41b8ee6113f0
Oleg Slobodskoi <oleg008@gmail.com>
2017-12-11 16:43:08 +0100

    lint bench

--- benchmark/tests/to-css-value.js
+++ benchmark/tests/to-css-value.js
@@ -7,41 +7,41 @@ const doubleArrayImportant = [['10px', '20px'], '!important']
 const string = '10px 20px !important'
 
 suite('toCssValue single array', () => {
-  benchmark('array', () => {
+  benchmark('array', () => (
     toCssValue(singleArray) === 'red'
-  })
+  ))
 
-  benchmark('string', () => {
+  benchmark('string', () => (
     toCssValue(string) === '10px 20px !important'
-  })
+  ))
 })
 
 suite('toCssValue signle array with important', () => {
-  benchmark('array', () => {
+  benchmark('array', () => (
     toCssValue(singleArrayImportant) === 'red'
-  })
+  ))
 
-  benchmark('string', () => {
+  benchmark('string', () => (
     toCssValue(string) === '10px 20px !important'
-  })
+  ))
 })
 
 suite('toCssValue double array', () => {
-  benchmark('array', () => {
+  benchmark('array', () => (
     toCssValue(doubleArray) === 'red'
-  })
+  ))
 
-  benchmark('string', () => {
+  benchmark('string', () => (
     toCssValue(string) === '10px 20px !important'
-  })
+  ))
 })
 
 suite('toCssValue double array with important', () => {
-  benchmark('array', () => {
+  benchmark('array', () => (
     toCssValue(doubleArrayImportant) === '10px 20px !important'
-  })
+  ))
 
-  benchmark('string', () => {
+  benchmark('string', () => (
     toCssValue(string) === '10px 20px !important'
-  })
+  ))
 })
