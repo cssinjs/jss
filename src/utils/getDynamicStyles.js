80ae742d59d6b2eff5b7d6226df61ee953ad4a03
Oleg Slobodskoi <oleg008@gmail.com>
2018-02-12 13:04:42 +0100

    simplify getDynamicStyles

--- src/utils/getDynamicStyles.js
+++ src/utils/getDynamicStyles.js
@@ -1,29 +1,24 @@
 /**
  * Extracts a styles object with only props that contain function values.
  */
-export default (styles: Object): Object | null => {
-  // eslint-disable-next-line no-shadow
-  function extract(styles: Object): Object {
-    let to = null
+export default function getDynamicStyles(styles: Object): Object | null {
+  let to = null
 
-    for (const key in styles) {
-      const value = styles[key]
-      const type = typeof value
+  for (const key in styles) {
+    const value = styles[key]
+    const type = typeof value
 
-      if (type === 'function') {
+    if (type === 'function') {
+      if (!to) to = {}
+      to[key] = value
+    } else if (type === 'object' && value !== null && !Array.isArray(value)) {
+      const extracted = getDynamicStyles(value)
+      if (extracted) {
         if (!to) to = {}
-        to[key] = value
-      } else if (type === 'object' && value !== null && !Array.isArray(value)) {
-        const extracted = extract(value)
-        if (extracted) {
-          if (!to) to = {}
-          to[key] = extracted
-        }
+        to[key] = extracted
       }
     }
-
-    return to
   }
 
-  return extract(styles)
+  return to
 }
