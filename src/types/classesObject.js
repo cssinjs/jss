af49fe2ca90d536948d390792a934cbdd8a9361e
PolGuixe <pol.guixe@gmail.com>
2018-06-20 18:03:34 +0200

    defined classesObject type

---
+++ src/types/classesObject.js
@@ -0,0 +1,1 @@
+export type classesObject = {[string]: string}
--- src/types/index.js
+++ src/types/index.js
@@ -3,3 +3,4 @@
 export * from './observable.js'
 export * from './cssom.js'
 export * from './jss.js'
+export * from './classesObject.js'
