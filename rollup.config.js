0a9bbbbc2f19f61b4becae5e949d0b68969424c6
Oleg Isonen <oleg008@gmail.com>
2018-06-24 12:18:16 +0200

    Use rollup globals plugin #736 (#741)
    * use rollup globals plugin #736
    
    * add ignoreGlobal to commonjs plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
 import {sizeSnapshot} from 'rollup-plugin-size-snapshot'
+import globals from 'rollup-plugin-node-globals'
 import pkg from './package.json'
 
 const matchSnapshot = process.env.SNAPSHOT === 'match'
@@ -29,8 +30,9 @@ export default [
     },
     plugins: [
       nodeResolve(),
-      commonjs({include: '**/node_modules/**'}),
+      commonjs({include: '**/node_modules/**', ignoreGlobal: true}),
       babel(getBabelOptions()),
+      globals(),
       replace({
         'process.env.NODE_ENV': JSON.stringify('development'),
         __VERSION__: JSON.stringify(pkg.version)
@@ -49,8 +51,9 @@ export default [
     },
     plugins: [
       nodeResolve(),
-      commonjs({include: '**/node_modules/**'}),
+      commonjs({include: '**/node_modules/**', ignoreGlobal: true}),
       babel(getBabelOptions()),
+      globals(),
       replace({
         'process.env.NODE_ENV': JSON.stringify('production'),
         __VERSION__: JSON.stringify(pkg.version)
