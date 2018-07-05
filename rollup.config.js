11425ee02c0f6a6c14650d7527c4fb5455be1f58
Oleg Isonen <oleg008@gmail.com>
2018-07-05 13:35:48 +0200

    deduplicate rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
@@ -8,64 +9,50 @@ import globals from 'rollup-plugin-node-globals'
 import pkg from './package.json'
 
 const matchSnapshot = process.env.SNAPSHOT === 'match'
+const packagesPath = path.join(__dirname, 'packages')
 
-const input = matchSnapshot ? './packages/jss/src/index.js' : './src/index.js'
-
-const getBabelOptions = () => ({
-  exclude: '**/node_modules/**',
-  babelrc: false,
-  presets: [['env', {modules: false}], 'stage-0', 'flow'],
-  plugins: ['external-helpers']
-})
+const base = {
+  input: path.join(packagesPath, 'jss/src/index.js'),
+  plugins: [
+    nodeResolve(),
+    commonjs({include: '**/node_modules/**', ignoreGlobal: true}),
+    babel({
+      exclude: '**/node_modules/**',
+      babelrc: false,
+      presets: [['env', {modules: false}], 'stage-0', 'flow'],
+      plugins: ['external-helpers']
+    }),
+    globals(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      __VERSION__: JSON.stringify(pkg.version)
+    }),
+    sizeSnapshot({
+      matchSnapshot,
+      snapshotPath: path.join(packagesPath, 'jss/.size-snapshot.json')
+    })
+  ]
+}
 
 export default [
   {
-    input,
+    ...base,
     output: {
       file: 'dist/jss.js',
       format: 'umd',
       sourcemap: true,
       exports: 'named',
       name: pkg.name
-    },
-    plugins: [
-      nodeResolve(),
-      commonjs({include: '**/node_modules/**', ignoreGlobal: true}),
-      babel(getBabelOptions()),
-      globals(),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-        __VERSION__: JSON.stringify(pkg.version)
-      }),
-      sizeSnapshot({
-        matchSnapshot,
-        snapshotPath: './packages/jss/.size-snapshot.json'
-      })
-    ]
+    }
   },
-
   {
-    input,
+    ...base,
+    plugins: [...base.plugins, uglify()],
     output: {
       file: 'dist/jss.min.js',
       format: 'umd',
       exports: 'named',
       name: pkg.name
-    },
-    plugins: [
-      nodeResolve(),
-      commonjs({include: '**/node_modules/**', ignoreGlobal: true}),
-      babel(getBabelOptions()),
-      globals(),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        __VERSION__: JSON.stringify(pkg.version)
-      }),
-      sizeSnapshot({
-        matchSnapshot,
-        snapshotPath: './packages/jss/.size-snapshot.json'
-      }),
-      uglify()
-    ]
+    }
   }
 ]
