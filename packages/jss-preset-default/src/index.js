54c7f2b565d55f665761f00380630527ea85af9e
Henri Beck <henribeck.dev@gmail.com>
2018-07-15 01:19:39 +0200

    Added jss-preset-default

---
+++ packages/jss-preset-default/src/index.js
@@ -0,0 +1,29 @@
+import functionPlugin from 'jss-plugin-syntax-rule-value-function'
+import observablePlugin from 'jss-plugin-syntax-rule-value-observable'
+import template from 'jss-plugin-syntax-template'
+import global from 'jss-plugin-syntax-global'
+import extend from 'jss-plugin-syntax-extend'
+import nested from 'jss-plugin-syntax-nested'
+import compose from 'jss-plugin-syntax-compose'
+import camelCase from 'jss-plugin-syntax-camel-case'
+import defaultUnit from 'jss-plugin-syntax-default-unit'
+import expand from 'jss-plugin-syntax-expand'
+import vendorPrefixer from 'jss-plugin-vendor-prefixer'
+import propsSort from 'jss-plugin-props-sort'
+
+export default (options = {}) => ({
+  plugins: [
+    functionPlugin(options.function),
+    observablePlugin(options.observable),
+    template(options.template),
+    global(options.global),
+    extend(options.extend),
+    nested(options.nested),
+    compose(options.compose),
+    camelCase(options.camelCase),
+    defaultUnit(options.defaultUnit),
+    expand(options.expand),
+    vendorPrefixer(options.vendorPrefixer),
+    propsSort(options.propsSort)
+  ]
+})
