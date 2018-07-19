d1ce11bf04ebf189bc1269b5c8db3795c5f80b96
Henri Beck <henribeck.dev@gmail.com>
2018-07-19 22:11:55 +0200

    Added typing to the compose plugin

--- packages/jss-plugin-syntax-compose/src/index.js
+++ packages/jss-plugin-syntax-compose/src/index.js
@@ -1,4 +1,6 @@
+// @flow
 import warning from 'warning'
+import type {Plugin, StyleSheet} from 'jss'
 
 /**
  * Set selector.
@@ -26,7 +28,9 @@ function registerClass(rule, className) {
     return registerClass(rule, className.split(' '))
   }
 
-  const {parent} = rule.options
+  const {parent} = ((rule.options: any): {parent: StyleSheet})
+
+  // parent = ((parent: any): StyleSheet)
 
   // It is a ref to a local rule.
   if (className[0] === '$') {
@@ -47,7 +51,7 @@ function registerClass(rule, className) {
     return true
   }
 
-  rule.options.parent.classes[rule.key] += ` ${className}`
+  parent.classes[rule.key] += ` ${className}`
 
   return true
 }
@@ -58,7 +62,7 @@ function registerClass(rule, className) {
  * @param {Rule} rule
  * @api public
  */
-export default function jssCompose() {
+export default function jssCompose(): Plugin {
   function onProcessStyle(style, rule) {
     if (!style.composes) return style
     registerClass(rule, style.composes)
