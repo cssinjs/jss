b2ebc65c157f2cff0ff50e1995cdf10698454a85
Henri Beck <henribeck.dev@gmail.com>
2018-07-23 21:34:33 +0200

    Added typings to the syntax template plugin

--- packages/jss-plugin-syntax-template/src/index.js
+++ packages/jss-plugin-syntax-template/src/index.js
@@ -1,11 +1,14 @@
+// @flow
+import {type Plugin} from 'jss'
 import parse from './parse'
 
 const onProcessRule = rule => {
   if (typeof rule.style === 'string') {
+    // $FlowFixMe: We can safely assume that rule has the style property
     rule.style = parse(rule.style)
   }
 }
 
-export default function templatePlugin() {
+export default function templatePlugin(): Plugin {
   return {onProcessRule}
 }
