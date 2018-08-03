3504f93bb9679682a3f56be8373761293b1fc130
Henri Beck <henribeck.dev@gmail.com>
2018-08-03 20:26:09 +0200

    Added typing to jss-preset-default

--- packages/jss-preset-default/src/index.js
+++ packages/jss-preset-default/src/index.js
@@ -1,29 +1,32 @@
+// @flow
 import functions from 'jss-plugin-syntax-rule-value-function'
 import observable from 'jss-plugin-syntax-rule-value-observable'
 import template from 'jss-plugin-syntax-template'
 import global from 'jss-plugin-syntax-global'
-// import extend from 'jss-plugin-syntax-extend'
+import extend from 'jss-plugin-syntax-extend'
 import nested from 'jss-plugin-syntax-nested'
 import compose from 'jss-plugin-syntax-compose'
 import camelCase from 'jss-plugin-syntax-camel-case'
-// import defaultUnit from 'jss-plugin-syntax-default-unit'
-// import expand from 'jss-plugin-syntax-expand'
+import defaultUnit, {type Options as DefaultUnitOptions} from 'jss-plugin-syntax-default-unit'
+import expand from 'jss-plugin-syntax-expand'
 import vendorPrefixer from 'jss-plugin-vendor-prefixer'
 import propsSort from 'jss-plugin-props-sort'
 
-export default (options = {}) => ({
+type Options = {defaultUnit?: DefaultUnitOptions}
+
+export default (options: Options = {}) => ({
   plugins: [
-    functions(options.function),
-    observable(options.observable),
-    template(options.template),
-    global(options.global),
-    // extend(options.extend),
-    nested(options.nested),
-    compose(options.compose),
-    camelCase(options.camelCase),
-    // defaultUnit(options.defaultUnit),
-    // expand(options.expand),
-    vendorPrefixer(options.vendorPrefixer),
-    propsSort(options.propsSort)
+    functions(),
+    observable(),
+    template(),
+    global(),
+    extend(),
+    nested(),
+    compose(),
+    camelCase(),
+    defaultUnit(options.defaultUnit),
+    expand(),
+    vendorPrefixer(),
+    propsSort()
   ]
 })
