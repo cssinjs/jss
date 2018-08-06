f9162d19077137f349bb855825dab6f12332e5e9
Henri Beck <henribeck.dev@gmail.com>
2018-08-06 19:58:27 +0200

    Added some tests

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,8 @@ module.exports = config => {
       './packages/react-jss/src/index.test.js',
       './packages/react-jss/src/compose.test.js',
       './packages/react-jss/src/JssProvider.test.js',
-      './packages/react-jss/src/injectSheet.test.js'
+      './packages/react-jss/src/injectSheet.test.js',
+      'packages/jss-keyframes-helper/src/index.test.js'
     ],
     preprocessors: {
       'node_modules/raf/polyfill.js': ['webpack'],
