5f1c1e869715ef3bc00b86d8c53afa2807a3f344
Henri Beck <henribeck.dev@gmail.com>
2018-08-12 17:28:39 +0200

    Improved Context Type

--- packages/react-jss/src/types.js
+++ packages/react-jss/src/types.js
@@ -40,8 +40,8 @@ export type InnerProps = {
 export type Context = {
   '64a55d578f856d258dc345b094a2a2b3'?: Jss,
   'd4bd0baacbc52bbd48bbb9eb24344ecd'?: SheetsRegistry,
-  'b768b78919504fba9de2c03545c5cd3a'?: { [key: string]: SheetsManager },
-  '6fc570d6bd61383819d0f9e7407c452d': StyleSheetFactoryOptions,
+  'b768b78919504fba9de2c03545c5cd3a'?: { [key: number]: SheetsManager },
+  '6fc570d6bd61383819d0f9e7407c452d': StyleSheetFactoryOptions & { disableStylesGeneration?: boolean },
 }
 export type OuterProps<IP, C> = IP & {innerRef: (instance: ElementRef<C> | null) => void}
 export type Theme = {}
