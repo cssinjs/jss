6a76f4fe3189c11d04e9f68cdb873a4b827c6b26
Oleg Isonen <oleg008@gmail.com>
2018-09-16 13:00:49 +0200

    temp fix benchmark

--- karma.conf.js
+++ karma.conf.js
@@ -65,8 +65,8 @@ module.exports = config => {
       browsers: ['Chrome'],
       frameworks: ['benchmark'],
       // Using a fixed position for a file name, m.b. should use an args parser later.
-      files: [process.argv[4] || 'benchmark/**/*.js'],
-      preprocessors: {'benchmark/**/*.js': ['webpack']},
+      files: [process.argv[4] || 'packages/jss/benchmark/**/*.js'],
+      preprocessors: {'packages/jss/benchmark/**/*.js': ['webpack']},
       reporters: ['benchmark'],
       // Some tests are slow.
       browserNoActivityTimeout: 20000
