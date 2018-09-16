5c51ba21d19089f043161406a30a18ae8e1add03
Bogdan Chadkin <trysound@yandex.ru>
2018-09-16 12:19:02 +0300

    Migrate to yarn
    One of NPM maintainers shows some disrespect to others which kills a
    trust to them.
    
    Yarn is more stable client with more full set of features so I suggest
    to migrate to it.
    
    As you can see yarn.lock is two times smaller because of less verbose
    syntax. Locfile is not required for every package in monorepo, thanks to
    yarn workspaces.

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const shell = require('shelljs')
 const chalk = require('chalk')
-const log = require('npmlog')
 
 const {build} = require('./build')
 const {getPackageJson} = require('./get-package-json')
@@ -9,11 +8,11 @@ const {getPackageJson} = require('./get-package-json')
 const pkg = getPackageJson()
 
 function logError(type) {
-  log.error(`FAILED to ${type}: ${chalk.bold(`${pkg.name}@${pkg.version}`)}`)
+  console.error(`FAILED to ${type}: ${chalk.bold(`${pkg.name}@${pkg.version}`)}`)
 }
 
 shell.rm('-rf', './dist/')
 shell.cp(path.join(__dirname, '..', 'LICENSE'), './')
 build({errorCallback: () => logError('build')})
 
-log.info(chalk.gray(`Built: ${chalk.bold(`${pkg.name}@${pkg.version}`)}`))
+console.info(chalk.gray(`Built: ${chalk.bold(`${pkg.name}@${pkg.version}`)}`))
