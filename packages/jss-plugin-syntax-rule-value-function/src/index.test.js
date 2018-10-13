171902b9c3767e3e7eb23502785681d6bd68debd
Henri Beck <henribeck.dev@gmail.com>
2018-10-13 16:17:28 +0200

    Added tests for using jss-plugin-syntax-expand with function rules and values

--- packages/jss-plugin-syntax-rule-value-function/src/index.test.js
+++ packages/jss-plugin-syntax-rule-value-function/src/index.test.js
@@ -1,5 +1,6 @@
 import './function-rules.test'
 import './function-values.test'
+import './plugin-expand.test'
 import './plugin-nested.test'
 import './plugin-global.test'
 import './plugin-compose.test'
