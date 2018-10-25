64178e68aa95a87949669ef2f8454cc7a0ccd617
Henri Beck <henribeck.dev@gmail.com>
2018-10-25 14:11:21 +0200

    Removed logs because we can't output nice logs

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const shell = require('shelljs')
-const log = require('./log')
 
 shell.rm('-rf', './dist/')
 shell.cp(path.join(__dirname, '..', 'LICENSE'), './')
@@ -9,9 +8,5 @@ const rollup = path.join(__dirname, '../node_modules/.bin/rollup')
 const {code} = shell.exec(`${rollup} --config "../../rollup.config.js"`)
 
 if (code !== 0) {
-  log('error', 'Build failed')
-
   shell.exit(code)
 }
-
-log('success', 'Build succeeded')
--- scripts/match-snapshot.js
+++ scripts/match-snapshot.js
@@ -1,6 +1,5 @@
 const shell = require('shelljs')
 const path = require('path')
-const log = require('./log')
 
 const crossEnv = path.join(__dirname, '../node_modules/.bin/cross-env')
 const rollup = path.join(__dirname, '../node_modules/.bin/rollup')
@@ -9,9 +8,5 @@ const {code} = shell.exec(
 )
 
 if (code !== 0) {
-  log('error', "Snapshots didn't match")
-
   shell.exit(code)
 }
-
-log('success', 'Snapshots matched')
