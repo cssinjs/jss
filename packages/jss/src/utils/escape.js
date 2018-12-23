1307297e3b4c69eb2fec725b2e6560712b163e4f
Oleg Isonen <oleg008@gmail.com>
2018-12-23 23:12:08 +0100

    global.CSS might not exist in the browser and def. doesn't in node

--- packages/jss/src/utils/escape.js
+++ packages/jss/src/utils/escape.js
@@ -2,14 +2,12 @@ const env = process.env.NODE_ENV
 
 const escapeRegex = /([[\].#*$><+~=|^:(),"'`])/g
 
+const nativeEscape = typeof CSS !== 'undefined' && CSS.escape
+
 export default str => {
   // We don't need to escape it in production, because we are not using user's
   // input for selectors, we are generating a valid selector.
   if (env === 'production') return str
 
-  if (CSS && CSS.escape) {
-    return CSS.escape(str)
-  }
-
-  return str.replace(escapeRegex, '\\$1')
+  return nativeEscape ? nativeEscape(str) : str.replace(escapeRegex, '\\$1')
 }
