66cf1b64948a4e8639484f6f6cf9bb5a7bd9392f
Oleg Isonen <oleg008@gmail.com>
2018-12-23 23:56:09 +0100

    inline all dev code

--- packages/jss/src/utils/escape.js
+++ packages/jss/src/utils/escape.js
@@ -1,13 +1,10 @@
-const env = process.env.NODE_ENV
-
-const escapeRegex = /([[\].#*$><+~=|^:(),"'`])/g
-
-const nativeEscape = typeof CSS !== 'undefined' && CSS.escape
-
 export default str => {
   // We don't need to escape it in production, because we are not using user's
   // input for selectors, we are generating a valid selector.
-  if (env === 'production') return str
+  if (process.env.NODE_ENV === 'production') return str
+
+  const escapeRegex = /([[\].#*$><+~=|^:(),"'`])/g
+  const nativeEscape = typeof CSS !== 'undefined' && CSS.escape
 
   return nativeEscape ? nativeEscape(str) : str.replace(escapeRegex, '\\$1')
 }
