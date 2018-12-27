9e4cde0fee16281cf586f072e7345de8c175be5c
Henri Beck <henribeck.dev@gmail.com>
2018-12-27 21:29:02 +0100

    Remove jsdoc comment

--- packages/react-jss/src/merge-classes.js
+++ packages/react-jss/src/merge-classes.js
@@ -1,14 +1,6 @@
 // @flow
 import type {Classes} from 'jss'
 
-/**
- * This merges to classes object.
- * When a property exists in both classes, it will not overwrite but merge the two classes.
- *
- * @param {Object} baseClasses - The base classes.
- * @param {Object} additionalClasses - The additional classes to merge on top of the base classes.
- * @return {Object} - Returns the merged classes object.
- */
 function mergeClasses(baseClasses: Classes, additionalClasses: Classes) {
   const combinedClasses = {...baseClasses}
 
