9d755387c295f8642bc7ee3c1307dbe49df3fdf6
Henri Beck <henribeck.dev@gmail.com>
2018-12-30 21:29:48 +0100

    Remove some unnecessary tests

--- packages/jss/tests/unit/escape.js
+++ packages/jss/tests/unit/escape.js
@@ -1,14 +1,7 @@
-/* eslint-disable no-underscore-dangle */
-
 import expect from 'expect.js'
 import escape from '../../src/utils/escape'
-import {resetSheets} from '../../../../tests/utils'
-
-const originalEscape = CSS.escape
 
 describe('Unit: jss - escape', () => {
-  beforeEach(resetSheets())
-
   describe('with CSS.escape', () => {
     it('should escape in development', () => {
       expect(escape('test()')).to.be('test\\(\\)')
@@ -18,21 +11,6 @@ describe('Unit: jss - escape', () => {
       process.env.NODE_ENV = 'production'
       expect(escape('test()')).to.be('test()')
       process.env.NODE_ENV = 'development'
-    })
-  })
-
-  describe('without CSS.escape', () => {
-    beforeEach(() => {
-      delete CSS.escape
-    })
-
-    afterEach(() => {
-      CSS.escape = originalEscape
-    })
-
-    it('should escape', () => {
-      escape('test()')
-      expect(escape('test()')).to.be('test\\(\\)')
     })
   })
 })
