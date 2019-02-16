705959b4c34d92bd38afdb2185cb9aec82127731
Henri Beck <henribeck.dev@gmail.com>
2019-02-16 17:29:14 +0100

    Revert "Format files"
    This reverts commit 63a5aab1

--- packages/react-jss/src/withStyles.js
+++ packages/react-jss/src/withStyles.js
@@ -79,8 +79,9 @@ export default function withStyles<Theme: {}, S: Styles<Theme>>(
       // $FlowFixMe
       static defaultProps = {...InnerComponent.defaultProps}
 
-      mergeClassesProp = memoize<Classes[], Classes>((sheetClasses, classesProp) =>
-        mergeClasses(sheetClasses, classesProp)
+      mergeClassesProp = memoize<Classes[], Classes>(
+        (sheetClasses, classesProp) =>
+          mergeClasses(sheetClasses, classesProp)
       )
 
       constructor(props: HOCProps<Theme, Props>) {
@@ -228,7 +229,7 @@ export default function withStyles<Theme: {}, S: Styles<Theme>>(
         const props = {
           ...rest,
           // $FlowFixMe
-          classes: classes ? this.mergeClassesProp(sheetClasses, classes) : sheetClasses
+          classes: classes ? this.mergeClassesProp(sheetClasses, classes) : sheetClasses,
         }
 
         if (innerRef) props.ref = innerRef
