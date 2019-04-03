7872f75307df5cba8bc617a13a2c5bd6dbb6d1fb
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-04-03 01:21:11 -0700

    Fix typspec for default unit plugin

--- packages/jss-plugin-default-unit/src/index.d.ts
+++ packages/jss-plugin-default-unit/src/index.d.ts
@@ -2,4 +2,4 @@ import {Plugin} from 'jss'
 
 export type Options = {[key: string]: string}
 
-export default function jssPluginSyntaxDefaultUnit(options: Options): Plugin
+export default function jssPluginSyntaxDefaultUnit(options?: Options): Plugin
