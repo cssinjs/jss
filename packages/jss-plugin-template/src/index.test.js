af3760bb68e791d2231fb6d0f5907cea99822b37
Oleg Isonen <oleg008@gmail.com>
2019-05-11 00:06:16 +0200

    support nesting

--- packages/jss-plugin-template/src/index.test.js
+++ packages/jss-plugin-template/src/index.test.js
@@ -4,13 +4,14 @@ import expect from 'expect.js'
 import {stripIndent} from 'common-tags'
 import {create} from 'jss'
 import sinon from 'sinon'
+import nested from 'jss-plugin-nested'
 import template from '.'
 
 const settings = {
   createGenerateId: () => rule => `${rule.key}-id`
 }
 
-describe('jss-plugin-template', () => {
+describe.only('jss-plugin-template', () => {
   let spy
   let jss
 
@@ -23,98 +24,97 @@ describe('jss-plugin-template', () => {
     console.warn.restore()
   })
 
-  describe('template literals', () => {
-    it('should convert a single single property/value', () => {
-      const sheet = jss.createStyleSheet({
-        a: `
+  it('should convert a single single property/value', () => {
+    const sheet = jss.createStyleSheet({
+      a: `
           color: red;
         `
-      })
-      expect(sheet.toString()).to.be(stripIndent`
+    })
+    expect(sheet.toString()).to.be(stripIndent`
         .a-id {
           color: red;
         }
       `)
-    })
+  })
 
-    it('should parse multiple props/values', () => {
-      const sheet = jss.createStyleSheet({
-        a: `
+  it('should parse multiple props/values', () => {
+    const sheet = jss.createStyleSheet({
+      a: `
           color: red;
           float: left;
         `
-      })
-      expect(sheet.toString()).to.be(stripIndent`
+    })
+    expect(sheet.toString()).to.be(stripIndent`
         .a-id {
           color: red;
           float: left;
         }
       `)
-      expect(spy.callCount).to.be(0)
+    expect(spy.callCount).to.be(0)
+  })
+
+  it('should warn when there is no colon found', () => {
+    jss.createStyleSheet({
+      a: 'color red;'
     })
 
-    it('should warn when there is no colon found', () => {
-      jss.createStyleSheet({
-        a: 'color red;'
-      })
+    expect(spy.callCount).to.be(1)
+    expect(spy.args[0][0]).to.be('Warning: [JSS] Missing colon in "color red".')
+  })
 
-      expect(spy.callCount).to.be(1)
-      expect(spy.calledWithExactly('Warning: [JSS] Malformed CSS string "color red;"')).to.be(true)
-    })
-
-    it('should strip spaces', () => {
-      const sheet = jss.createStyleSheet({
-        a: `
+  it('should strip spaces', () => {
+    const sheet = jss.createStyleSheet({
+      a: `
             color:     red   ;
             float:   left   ;
         `
-      })
-      expect(sheet.toString()).to.be(stripIndent`
+    })
+    expect(sheet.toString()).to.be(stripIndent`
         .a-id {
           color: red;
           float: left;
         }
       `)
-    })
+  })
 
-    it('should allow skiping last semicolon', () => {
-      const sheet = jss.createStyleSheet({
-        a: `
+  it('should allow skiping last semicolon', () => {
+    const sheet = jss.createStyleSheet({
+      a: `
           color: red;
           float: left
         `
-      })
-      expect(sheet.toString()).to.be(stripIndent`
+    })
+    expect(sheet.toString()).to.be(stripIndent`
         .a-id {
           color: red;
           float: left;
         }
       `)
-    })
+  })
 
-    it('should support @media', () => {
-      const sheet = jss.createStyleSheet({
-        '@media print': {
-          button: 'color: black'
-        }
-      })
-      expect(sheet.toString()).to.be(stripIndent`
+  it('should support @media', () => {
+    const sheet = jss.createStyleSheet({
+      '@media print': {
+        button: 'color: black'
+      }
+    })
+    expect(sheet.toString()).to.be(stripIndent`
         @media print {
           .button-id {
             color: black;
           }
         }
       `)
-    })
+  })
 
-    it('should support @keyframes', () => {
-      const sheet = jss.createStyleSheet({
-        '@keyframes a': {
-          from: 'opacity: 0',
-          to: 'opacity: 1'
-        }
-      })
-      expect(sheet.toString()).to.be(stripIndent`
+  it('should support @keyframes', () => {
+    const sheet = jss.createStyleSheet({
+      '@keyframes a': {
+        from: 'opacity: 0',
+        to: 'opacity: 1'
+      }
+    })
+    expect(sheet.toString()).to.be(stripIndent`
         @keyframes keyframes-a-id {
           from {
             opacity: 0;
@@ -124,6 +124,34 @@ describe('jss-plugin-template', () => {
           }
         }
       `)
-    })
   })
+
+  it('should support nesting', () => {
+    jss = create(settings).use(template(), nested())
+
+    const sheet = jss.createStyleSheet({
+      a: `
+        color: green;
+        & .b {
+          color: red;
+        }
+      `
+    })
+    expect(sheet.toString()).to.be(stripIndent`
+        .a-id {
+          color: green;
+        }
+        .a-id .b {
+          color: red;
+        }
+      `)
+  })
+
+  it('should warn when opening curly brace is missing', () => {})
+  it('should warn when closing curly brace is missing', () => {})
+  it('should warn when closing curly brace is not on an own line', () => {})
+  it('should warn when closing curly brace is not provided', () => {})
+  it('should support multiple first level nested rules ', () => {})
+  it('should support multiple deeply nested rules', () => {})
+  it('should regular props after a nested rule', () => {})
 })
--- packages/jss-plugin-template/src/parse.js
+++ packages/jss-plugin-template/src/parse.js
@@ -1,7 +1,7 @@
 // @flow
 import warning from 'tiny-warning'
 
-const semiWithNl = /;\n/
+const semiWithNl = /[;\n]|[}\n]/
 
 /**
  * Naive CSS parser.
@@ -9,21 +9,51 @@ const semiWithNl = /;\n/
  * - Requires semicolon and new line after the value (except of last line)
  * - No nested rules support
  */
-export default (cssText: string) => {
+export default (cssText: string): Object => {
   const style = {}
   const split = cssText.split(semiWithNl)
+  let nestedRuleProp
+
   for (let i = 0; i < split.length; i++) {
     const decl = (split[i] || '').trim()
 
     if (!decl) continue
+
+    if (nestedRuleProp === undefined) {
+      const ampIndex = decl.indexOf('&')
+      // We have a nested rule.
+      if (ampIndex !== -1) {
+        const openCurlyIndex = decl.indexOf('{')
+        if (openCurlyIndex === -1) {
+          warning(false, `[JSS] Missing opening curly brace in "${decl}".`)
+          continue
+        }
+        nestedRuleProp = decl.substr(0, openCurlyIndex).trim()
+        style[nestedRuleProp] = {}
+        continue
+      }
+    } else {
+      const closeCurlyIndex = decl.indexOf('}')
+      if (closeCurlyIndex !== -1) {
+        nestedRuleProp = undefined
+        continue
+      }
+    }
+
     const colonIndex = decl.indexOf(':')
     if (colonIndex === -1) {
-      warning(false, `[JSS] Malformed CSS string "${decl}"`)
+      warning(false, `[JSS] Missing colon in "${decl}".`)
       continue
     }
+
     const prop = decl.substr(0, colonIndex).trim()
     const value = decl.substr(colonIndex + 1).trim()
-    style[prop] = value
+
+    if (nestedRuleProp) {
+      style[nestedRuleProp][prop] = value
+    } else {
+      style[prop] = value
+    }
   }
   return style
 }
