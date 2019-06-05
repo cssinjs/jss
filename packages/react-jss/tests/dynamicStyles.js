32bb698cbb4ba24f43b9da657974640bf9c94d6d
Oleg Isonen <oleg008@gmail.com>
2019-06-05 21:02:45 +0200

    generate dynamic styles test using HOC

--- packages/react-jss/tests/dynamicStyles.js
+++ packages/react-jss/tests/dynamicStyles.js
@@ -1,20 +1,34 @@
 /* eslint-disable react/prop-types */
 
-import {createUseStyles} from '../src'
+import {createUseStyles, withStyles} from '../src'
 import createDynamicStylesTests from '../test-utils/createDynamicStylesTests'
 
-const createStyledComponent = (styles, options = {}) => {
-  const useStyles = createUseStyles(styles, options)
-  const Comp = props => {
-    const classes = useStyles(props)
-    props.getClasses(classes)
-    return null
-  }
-  Comp.displayName = options.name
+describe('React-JSS: dynamic styles', () => {
+  describe('using createUseStyles', () => {
+    const createStyledComponent = (styles, options = {}) => {
+      const useStyles = createUseStyles(styles, options)
+      const Comp = props => {
+        const classes = useStyles(props)
+        props.getClasses(classes)
+        return null
+      }
+      Comp.displayName = options.name
+      return Comp
+    }
 
-  return Comp
-}
+    createDynamicStylesTests({createStyledComponent})
+  })
 
-describe('React-JSS: dynamic styles with createUseStyles', () => {
-  createDynamicStylesTests({createStyledComponent})
+  describe('using withStyles', () => {
+    const createStyledComponent = (styles, options = {}) => {
+      const Comp = props => {
+        props.getClasses(props.classes)
+        return null
+      }
+      Comp.displayName = options.name
+      return withStyles(styles, options)(Comp)
+    }
+
+    createDynamicStylesTests({createStyledComponent})
+  })
 })
