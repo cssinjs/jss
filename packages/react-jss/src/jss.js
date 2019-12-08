3d404c030d5ef322bd1641dcfb0ea1852ed4d2c8
yanxingzhe <yanxingzhe@pinduoduo.com>
2019-12-08 20:15:06 +0800

    feat: export defaultJss and add createJss

--- packages/react-jss/src/jss.js
+++ packages/react-jss/src/jss.js
@@ -1,5 +1,12 @@
 // @flow
-import {create} from 'jss'
+import {create, type JssOptions} from 'jss'
 import preset from 'jss-preset-default'
+
+export const createJss = (options?: Partial<JssOptions>) => {
+  return create({
+    ...preset(),
+    ...(options || {})
+  })
+}
 
 export default create(preset())
