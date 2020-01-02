49e29cb7e0e8d3d68c352161cc2d2e1b00bb39f0
Andrey Osiyuk <osiyuk@protonmail.com>
2020-01-02 14:54:03 +0300

    Add createCss export types

--- packages/css-jss/src/index.d.ts
+++ packages/css-jss/src/index.d.ts
@@ -1,7 +1,15 @@
-import {Style} from 'jss'
+import {Jss, Style} from 'jss'
 
 export {Style}
 
 export type StyleArg = Style | Array<Style>
 
 export type Css = (...args: StyleArg[]) => string
+
+type CreateCss = (jss?: Jss) => Css
+
+declare const create: CreateCss
+export {create}
+
+declare const createCss: ReturnType<CreateCss>
+export default createCss
