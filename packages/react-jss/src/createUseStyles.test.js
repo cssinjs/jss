21def2d55771ed4dc06dfcc931e49e7a5a907e55
yeliex <yeliex@yeliex.com>
2020-01-13 11:17:16 +0800

    test: update test for options.generateId

--- packages/react-jss/src/createUseStyles.test.js
+++ packages/react-jss/src/createUseStyles.test.js
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
 
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import expect from 'expect.js'
 import createUseStyles from './createUseStyles'
 import createBasicTests from '../test-utils/createBasicTests'
 
@@ -14,4 +17,29 @@ const createStyledComponent = (styles, options) => {
 
 describe('React-JSS: createUseStyles', () => {
   createBasicTests({createStyledComponent})
+
+  describe('options.generateId render as expect', () => {
+    it('use passed options.generateId', () => {
+      const useCustom = styles =>
+        createUseStyles(styles, {
+          generateId: rule => `ui-${rule.key}`
+        })
+
+      const useStyles = useCustom({
+        button: {
+          color: 'red'
+        }
+      })
+
+      const StyledComponent = props => {
+        const classes = useStyles(props)
+
+        return <button className={classes.button} type="button" />
+      }
+
+      const renderer = TestRenderer.create(<StyledComponent />)
+
+      expect(renderer.toJSON().props.className).equal('ui-button')
+    })
+  })
 })
