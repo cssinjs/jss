060a7a3e0c69bb597ae88b3e5c499566fa92a33f
Kate Sills <kate@agoric.com>
2021-02-04 15:34:04 -0800

    chore: edit globalThis.js to work under SES

--- packages/jss/src/utils/globalThis.js
+++ packages/jss/src/utils/globalThis.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 // https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
-export default (typeof window != 'undefined' && window.Math == Math
-  ? window
-  : typeof self != 'undefined' && self.Math == Math
-    ? self
-    : Function('return this')())
+export default (typeof globalThis !== 'undefined'
+  ? globalThis
+  : typeof window !== 'undefined' && window.Math === Math
+    ? window
+    : typeof self !== 'undefined' && self.Math === Math
+      ? self
+      : Function('return this')())
