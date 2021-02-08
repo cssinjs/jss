a4f58de310c6149811410ad5639e841cf4b5c5dd
Kate Sills <kate@agoric.com>
2021-02-08 11:43:02 -0800

    chore: add comment

--- packages/jss/src/utils/globalThis.js
+++ packages/jss/src/utils/globalThis.js
@@ -1,5 +1,17 @@
 /* eslint-disable */
-// https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
+
+/**
+ * Now that `globalThis` is available on most platforms
+ * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis#browser_compatibility)
+ * we check for `globalThis` first. `globalThis` is necessary for jss
+ * to run in Agoric's secure version of JavaScript (SES). Under SES,
+ * `globalThis` exists, but `window`, `self`, and `Function('return
+ * this')()` are all undefined for security reasons.
+ *
+ * https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
+ */
+
+//
 export default (typeof globalThis !== 'undefined'
   ? globalThis
   : typeof window !== 'undefined' && window.Math === Math
